7e794331ae35c7577b1328015ee4fd04788cd594
souravpromantia <sourav.sb@promantia.com>
2024-07-29 16:11:54 +0530

    Corrected the family address field visibility issue

--- health_care/family_adoption_program/doctype/family_details/family_details.py
+++ health_care/family_adoption_program/doctype/family_details/family_details.py
@@ -1,9 +1,9 @@
 # Copyright (c) 2024, Promantia and contributors
 # For license information, please see license.txt
 
-# from frappe.model.document import Document
-# from frappe.contacts.address_and_contact import load_address_and_contact
+from frappe.model.document import Document
+from frappe.contacts.address_and_contact import load_address_and_contact
 
-# class FamilyDetails(Document):
-#     def onload(self):
-#         load_address_and_contact(self)
+class FamilyDetails(Document):
+    def onload(self):
+        load_address_and_contact(self)
